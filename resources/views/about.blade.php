ccd64c3785d2d3bd6778ba0ae8d2163503f7db7f
rahul26021999 <rahul26021999@gmail.com>
2019-05-31 15:12:31 +0530

    myfirstcommit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title','about')
+@section('content')
+<h1>about me here</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab perspiciatis nemo dolore, praesentium illum architecto ipsum quam culpa? Obcaecati amet asperiores vitae eligendi voluptates modi eaque molestias rem architecto magnam.</p>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title','contact')
+@section('content')
+<h1>contact here</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, excepturi! Ipsum, ratione quo. Odio natus dignissimos tempora quod atque repellat neque dolor. Possimus rerum corporis culpa voluptas. Earum, fugit architecto?</p>
+@endsection
